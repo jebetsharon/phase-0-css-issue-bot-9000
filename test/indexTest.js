d283652c837492d229965d2178ee2f654173efe2
Sharon kosgei <jebetsharon089@gmail.com>
2025-02-22 22:22:56 +0300

    Fix CSS issues and update test cases

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,4 @@
-require ( './helpers.js' );
+require('./helpers.js');
 
 const fs = require("fs");
 const path = require("path");
@@ -25,7 +25,7 @@ describe("css/style.css", () => {
   it("corrects the 'body' background declaration", () => {
     const rule = findRule(css.cssRules, "body");
     const hint = "Incorrect background property for body";
-    expect(rule.style["background"], hint).to.eq("pink");
+    expect(rule.style["background-color"], hint).to.eq("pink"); // Changed from 'background' to 'background-color'
   });
 
   it("corrects the 'h1' font-size declaration", () => {
@@ -49,6 +49,6 @@ describe("css/style.css", () => {
   it("corrects the '#tasks .completed' background declaration", () => {
     const rule = findRule(css.cssRules, "#tasks .completed");
     const hint = "Incorrect background property for '#tasks .completed'";
-    expect(rule.style["background"], hint).to.eq("#ccc");
+    expect(rule.style["background-color"], hint).to.eq("#ccc"); // Changed from 'background' to 'background-color'
   });
 });
